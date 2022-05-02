ba49fa5da808965a60b6928963988e88f3504f87
Kortelainen <tuurihaukka92@hotmail.com>
2022-05-02 11:22:04 +0300

    Updates

---
+++ _app/pages/uses.md-4514b576.js
@@ -0,0 +1,19 @@
+import{S as z,i as B,s as Q,e as l,t as m,k as f,c as r,a as p,h as k,d as e,m as d,b as W,g as _,F as a,n as F}from"../chunks/index-df0c0336.js";function Y(N){let i,w,C,v,h,y,I,t,u,O,A,S,H,M,E,U,b,L,P,R,x,g,j,c,X=`<code class="language-js"><span class="token comment">// JavaScript goes here,</span>
+<span class="token comment">// And will be syntax-highlighted! COOL FOR CODE EXAMPLES</span>
+
+
+<span class="token operator">--</span><span class="token operator">-</span>
+# frontmatter goes here
+<span class="token operator">--</span><span class="token operator">-</span>
+<span class="token operator">&lt;</span>script<span class="token operator">></span>
+  <span class="token keyword">import</span> SomeComponent <span class="token keyword">from</span> <span class="token string">'$lib/components/SomeComponent.svelte'</span>
+<span class="token operator">&lt;</span><span class="token operator">/</span>script<span class="token operator">></span>
+
+# Markdown content here
+
+<span class="token operator">&lt;</span>SomeComponent <span class="token operator">/</span><span class="token operator">></span>
+
+More markdown _here_<span class="token operator">!</span>
+</code>`;return{c(){i=l("h1"),w=m("Uses"),C=f(),v=l("p"),h=l("strong"),y=m("Here\u2019s some stuff I use"),I=f(),t=l("ul"),u=l("li"),O=m("SvelteKit"),A=f(),S=l("li"),H=m("VS Code"),M=f(),E=l("li"),U=m("mdsvex"),b=f(),L=l("li"),P=m(`SASS
+sveltejs/adapter-static`),R=f(),x=l("li"),g=m("Emojis \u{1F60E}"),j=f(),c=l("pre"),this.h()},l(s){i=r(s,"H1",{});var o=p(i);w=k(o,"Uses"),o.forEach(e),C=d(s),v=r(s,"P",{});var K=p(v);h=r(K,"STRONG",{});var T=p(h);y=k(T,"Here\u2019s some stuff I use"),T.forEach(e),K.forEach(e),I=d(s),t=r(s,"UL",{});var n=p(t);u=r(n,"LI",{});var V=p(u);O=k(V,"SvelteKit"),V.forEach(e),A=d(n),S=r(n,"LI",{});var q=p(S);H=k(q,"VS Code"),q.forEach(e),M=d(n),E=r(n,"LI",{});var D=p(E);U=k(D,"mdsvex"),D.forEach(e),b=d(n),L=r(n,"LI",{});var G=p(L);P=k(G,`SASS
+sveltejs/adapter-static`),G.forEach(e),R=d(n),x=r(n,"LI",{});var J=p(x);g=k(J,"Emojis \u{1F60E}"),J.forEach(e),n.forEach(e),j=d(s),c=r(s,"PRE",{class:!0});var $=p(c);$.forEach(e),this.h()},h(){W(c,"class","language-js")},m(s,o){_(s,i,o),a(i,w),_(s,C,o),_(s,v,o),a(v,h),a(h,y),_(s,I,o),_(s,t,o),a(t,u),a(u,O),a(t,A),a(t,S),a(S,H),a(t,M),a(t,E),a(E,U),a(t,b),a(t,L),a(L,P),a(t,R),a(t,x),a(x,g),_(s,j,o),_(s,c,o),c.innerHTML=X},p:F,i:F,o:F,d(s){s&&e(i),s&&e(C),s&&e(v),s&&e(I),s&&e(t),s&&e(j),s&&e(c)}}}class ss extends z{constructor(i){super(),B(this,i,null,Y,Q,{})}}export{ss as default};
